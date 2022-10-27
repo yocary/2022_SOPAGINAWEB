9e828cde678ccbb09086718e9b0ad110e02c3240
angelvela0409 <miguelcurup55@gmail.com>
2022-10-27 16:50:47 -0600

    un cambio

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,5 +1,3 @@
-
-
 echo 'The following "npm" command (if executed) installs the "cross-env"'
 echo 'dependency into the local "node_modules" directory, which will ultimately'
 echo 'be stored in the Jenkins home directory. As described in'
